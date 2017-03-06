28479f14937775b24b96c068067fb2ffbfd55ebf
ncc <ncc>
2017-03-06 09:51:15 -0800

    Changes for Lab

---
+++ Bank Lab/Bank Lab/Customer.cpp
@@ -0,0 +1,18 @@
+#include "Customer.h"
+using namespace std;
+
+
+
+Customer::Customer(string firstName, string lastName) : _firstName(firstName), _lastName(lastName)
+{
+}
+
+
+Customer::~Customer()
+{
+}
+
+std::string Customer::getFullName()
+{
+	return _lastName + ", " + _firstName;
+}
