ce3f4d392f139fef1dc4181987e895ca281c7b6f
ncc <ncc>
2017-03-06 10:03:53 -0800

    Modified account class

---
+++ Bank Lab/Bank Lab/Account.cpp
@@ -0,0 +1,26 @@
+#include "Account.h"
+
+
+Account::Account(Customer customer, int accountNumber) : _accountOwner(customer), _accountNumber(accountNumber)
+{
+}
+
+Account::~Account()
+{
+}
+
+void Account::Deposit(int numberOfPennies)
+{
+	_balanceInPennies += numberOfPennies;
+
+}
+
+void Account::Withdraw(int numberOfPennies)
+{
+	_balanceInPennies -= numberOfPennies;
+}
+
+int Account::getBalance()
+{
+	return 0;
+}
---
+++ Bank Lab/Bank Lab/Account.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Customer.h"
+
+
+class Account
+{
+private:
+	int _accountNumber;
+	int _balanceInPennies;
+	Customer _accountOwner;
+public:
+	Account(Customer customer, int accountNumber);
+	~Account();
+
+	void Deposit(int numberOfPennies);
+	void Withdraw(int numberOfPennies);
+	int getBalance();
+};
+
